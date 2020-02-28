915ee427448941f0cc7f23cfa206e42a7420d600
Heido Nomm <heidonomm@gmail.com>
2020-02-28 12:04:18 +0000

    formatted code 4 higher reusability

--- client.go
+++ client.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"bytes"
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
-func ReserveSlot(port int, slotID int) []byte {
+func reserveSlot(port int, slotID int) []byte {
 	return RequestSingle(port, slotID, buildRequestReserve)
 }
 
-func CancelSlot(port int, slotID int) []byte {
+func cancelSlot(port int, slotID int) []byte {
 	return RequestSingle(port, slotID, buildRequestCancel)
 }
 
@@ -31,24 +33,37 @@ func RequestSingle(port int, slotID int, putRequest funcGetSingle) []byte {
 	body := putRequest(slotID)
 	url := fmt.Sprintf("http://jewel.cs.man.ac.uk:%d/queue/enqueue", port)
 
-	client := &http.Client{}
 	req, err := http.NewRequest(http.MethodPut, url, bytes.NewReader(body))
 	if err != nil {
 		panic(err)
 	}
 
+	client := &http.Client{}
+
 	req.Header.Add("Content-Type", "application/xml")
 	req.Header.Add("Accept", "application/xml")
 
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
+	var resp *http.Response
+	resp, _ = client.Do(req)
+	for resp.StatusCode >= 400 {
+		resp, _ = client.Do(req)
 	}
 
 	bodyRes, _ := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
 
-	return bodyRes
+	// bodyRes := repeatedExecutionWithReqObj(req)
+
+	var resUrl string
+	if err := xml.Unmarshal(bodyRes, &resUrl); err != nil {
+		panic(err)
+	}
+
+	getQueryUrl := fmt.Sprintf("%s?username=%s&password=%s", resUrl, Username, Password)
+	fmt.Println(getQueryUrl)
+	//get the response
+
+	return repeatedExecution(getQueryUrl)
 }
 
 func RequestMultiple(port int, getAll funcGetAll) []byte {
@@ -56,18 +71,76 @@ func RequestMultiple(port int, getAll funcGetAll) []byte {
 	body := getAll()
 	url := fmt.Sprintf("http://jewel.cs.man.ac.uk:%d/queue/enqueue", port)
 
-	client := &http.Client{}
+	// client := &http.Client{}
 	req, err := http.NewRequest(http.MethodPut, url, bytes.NewReader(body))
 	if err != nil {
 		panic(err)
 	}
+	client := &http.Client{}
 
 	req.Header.Add("Content-Type", "application/xml")
 	req.Header.Add("Accept", "application/xml")
 
-	resp, err := client.Do(req)
+	var resp *http.Response
+	resp, _ = client.Do(req)
+	for resp.StatusCode >= 400 {
+		resp, _ = client.Do(req)
+	}
+
+	bodyRes, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	// bodyRes := repeatedExecutionWithReqObj(req)
+
+	//parse the url to make the request to (url where server response is hidden)
+	var resUrl string
+	if err := xml.Unmarshal(bodyRes, &resUrl); err != nil {
+		panic(err)
+	}
+	getQueryUrl := fmt.Sprintf("%s?username=%s&password=%s", resUrl, Username, Password)
+	fmt.Println(getQueryUrl)
+	//get the response
+
+	return repeatedExecution(getQueryUrl)
+}
+
+func repeatedExecution(queryUrl string) []byte {
+
+	var respMult *http.Response
+
+	respMult, _ = http.Get(queryUrl)
+
+	counter := 1
+	var respBody []byte
+
+	for respMult.StatusCode != 200 {
+		respMult, _ = http.Get(queryUrl)
+
+		time.Sleep(time.Second)
+		fmt.Printf("\rTime failed: %d, statusCode: %d,  \n", counter, respMult.StatusCode)
+		counter++
+	}
+
+	b, err := ioutil.ReadAll(respMult.Body)
 	if err != nil {
 		panic(err)
+	}
+	respBody = b
+	fmt.Println(string(b))
+
+	return respBody
+}
+
+func repeatedExecutionWithReqObj(req *http.Request) []byte {
+	client := &http.Client{}
+
+	req.Header.Add("Content-Type", "application/xml")
+	req.Header.Add("Accept", "application/xml")
+
+	var resp *http.Response
+	resp, _ = client.Do(req)
+	for resp.StatusCode >= 400 {
+		resp, _ = client.Do(req)
 	}
 
 	bodyRes, _ := ioutil.ReadAll(resp.Body)
--- main.go
+++ main.go
@@ -3,42 +3,101 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"time"
 )
 
 const HotelPort = 3010
 const BandPort = 3020
 
+const (
+	waitforServer   = iota
+	reqAvailability = iota
+	reqSlot         = iota
+	reqBookings     = iota
+)
+
 func main() {
-	response := getFreeSlots(3020)
-	var url string
-	err := xml.Unmarshal(response, &url)
-	if err != nil {
-		fmt.Println("esimene")
-		panic(err)
-	}
-
-	query := fmt.Sprintf("%s?username=%s&password=%s", url, Username, Password)
-	fmt.Println(query)
-	time.Sleep(20000 * time.Millisecond)
-	resp, err := http.Get(query)
-	if err != nil {
-		fmt.Println("teine")
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("kolmas")
-		panic(err)
-	}
-
 	var availability ResponseAvailability
-	xml.Unmarshal(body, &availability)
+	availabilityBytes := getFreeSlots(HotelPort)
+	xml.Unmarshal(availabilityBytes, &availability)
 	fmt.Println(availability)
-	fmt.Println(resp.StatusCode)
-	fmt.Println(string(body))
+	// freeSlots := availability.Body
+	bestSlot := availability.Body[0]
+	fmt.Println(bestSlot)
+
+	responseBytes := reserveSlot(HotelPort, bestSlot)
+	fmt.Println(string(responseBytes))
+
+	bookingsBytes := getBookings(HotelPort)
+	fmt.Println(string(bookingsBytes))
 }
+
+// func main() {
+// 	response := getBookings(3020)
+// 	var url string
+// 	err := xml.Unmarshal(response, &url)
+// 	if err != nil {
+// 		fmt.Println("esimene")
+// 		panic(err)
+// 	}
+
+// 	query := fmt.Sprintf("%s?username=%s&password=%s", url, Username, Password)
+// 	fmt.Println(query)
+// 	time.Sleep(20000 * time.Millisecond)
+// 	resp, err := http.Get(query)
+// 	if err != nil {
+// 		fmt.Println("teine")
+// 		panic(err)
+// 	}
+// 	defer resp.Body.Close()
+
+// 	body, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		fmt.Println("kolmas")
+// 		panic(err)
+// 	}
+
+// 	var availability ResponseBookings
+// 	xml.Unmarshal(body, &availability)
+// 	fmt.Println(availability)
+// 	fmt.Println(resp.StatusCode)
+// 	fmt.Println(string(body))
+// }
+
+// response := getBookings(3020)
+// var url string
+// err := xml.Unmarshal(response, &url)
+// if err != nil {
+// 	fmt.Println("esimene")
+// 	panic(err)
+// }
+// state := make(chan int)
+// select {
+// 	tocker
+// }
+// for {
+// 	select <-ticker {
+// 		switch <-state {
+
+// 		}
+// 	}
+
+// }
+
+// ticker := time.NewTicker(500 * time.Millisecond)
+// done := make(chan bool)
+
+// go func() {
+// 	for {
+// 		select {
+// 		case <-done:
+// 			return
+// 		case t := <-ticker.C:
+// 			fmt.Println("Tick at", t)
+// 		}
+// 	}
+// }()
+
+// time.Sleep(1600 * time.Millisecond)
+// ticker.Stop()
+// done <- true
+// fmt.Println("Ticker stopped")
